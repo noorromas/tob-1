9d23f7d7d8fca91be993c06de587febf077ce22c
yaf3iaden <42390257+noorromas@users.noreply.github.com>
2025-07-03 03:25:04 +0300

    Add files via upload

---
+++ tob-1.js
@@ -0,0 +1,99 @@
+$(`<style>
+ul.nav.nav-tabs.fl li a {
+    text-align: center;
+    padding: 0px 0!important;
+    border: 0 solid #4c4b4b;
+    color:#2E576E;
+    border-radius: 25px;
+    background: #fff;
+}
+ul.nav.nav-tabs.fl li {
+    width: 33.3%;
+    margin-left: 0;
+    border-radius: 50px;
+    margin-bottom: 5px!important;
+}
+div#l2 input#pass1, div#l2 input#u2, div#l3 input#pass2, div#l3 input#u3 {
+    padding: 0px 0px!important;
+    border-radius: 25px!important;
+    border: 1px solid #e1e0e0;
+    text-align: center;
+    box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 1px 2px rgb(107 32 49);
+    border: 1px solid white;
+    height: 27px!important;
+    border: 1px solid #dedede!important;
+    text-align: center;
+    width: 49%!important;
+    padding: 0!important;
+    float: right;
+    margin: 2px 3px 2px 0;
+}
+div#l2 button {
+	    border-radius: 24px!important;
+    height: 27px;
+    padding: 0px 5px;
+    width: 98%;
+    margin-left: 4px;
+    box-shadow: inset 0 0px 0 rgb(255 255 255), 0 2px 0px rgb(252 199 209);
+    border-color: #FFFFFF!important;
+}.nav-tabs{
+	background-color: #bfbfbfa6!important;
+	border-radius: 24px!important;
+	margin-top: 2px!important;
+	float: right;
+	padding: 3px 1px;
+	border-top: 3px solid;
+	border-color: #2E576E;
+	height: 30px!important;
+	}
+
+#u1 {
+	padding: 0px 0px!important;
+	margin-left: -6px;
+	border-radius: 25px!important;
+	border: 1px solid #e1e0e0;
+	text-align: center;
+	box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 1px 2px rgb(107 32 49);
+	border: 1px solid white;
+	height: 27px!important;
+	width: 100%!important;
+	margin-top: 1px!important;
+
+}
+#chats{background-color:#d4cfd2!important;background-image:url(https://up6.cc/2025/07/175150184952751.png);background-size:100%}#wall .uzr.d-flex.mm{border:1px solid transparent!important}#d2bc{background-image:url(https://up6.cc/2025/07/175150211381221.png);background-size:100%}#wall .fitimg.u-pic.borderg{height:52px!important;border:1px solid;border-radius:100px;box-shadow:0 0 0 #000000,inset 0 0 4px #000000,0 0 0 0 #000;margin-top:2px;margin-right:2px}
+
+}
+div#l1 input#u1, div#l3 button,div#l1 button {
+    border-radius: 24px!important;
+    height: 27px;
+    padding: 0px 5px;
+    width: 98%;
+    margin-left: 4px;
+    box-shadow: inset 0 0px 0 rgb(255 255 255), 0 2px 0px rgb(252 199 209);
+    border-color: #FFFFFF!important;
+}
+#tlogins .fa.fa-eye {
+    position: relative;
+    margin: -30px 17px 2px 0px!important;
+    color: #fff!important;
+}
+
+
+
+button {
+  border-radius: 5px !important;
+   
+   
+}
+
+
+.label {
+  border-radius: 5px !important;
+ 
+}
+
+.label, .btn {
+  border-radius: 5px !important;
+ 
+
+
